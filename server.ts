b031fa434dc688dc6b01cccf82376df08497f045
Inon Bercu <inonbercu@Inons-MacBook-Pro.local>
2022-06-16 08:20:06 +0300

    typescript set-up

--- server.ts
+++ server.ts
@@ -1,1 +1,16 @@
-console.log("first");
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`🟢 app listening on port http://localhost:${port}`);
+});
