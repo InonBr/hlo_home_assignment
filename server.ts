ed35cd020fd65a94e4a7b6bdbc9eae54be9675c6
Inon Bercu <inonbr@gmail.com>
2022-06-16 22:38:53 +0300

    fixed some stuff for heroku

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import usersRouters from "./routes/usersRoutes/usersRoutes";
 import messagesRoutes from "./routes/messagesRoutes/messagesRoutes";
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
