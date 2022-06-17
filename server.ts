0e0d1599b17a9358578365b951d389a72387dcda
Inon Bercu <inonbr@gmail.com>
2022-06-17 06:53:30 +0300

    fixed yarn start commend

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
-import * as express from "express";
-import * as cors from "cors";
+import express from "express";
+import cors from "cors";
 import connectDB from "./db/db";
 import usersRouters from "./routes/usersRoutes/usersRoutes";
 import messagesRoutes from "./routes/messagesRoutes/messagesRoutes";
